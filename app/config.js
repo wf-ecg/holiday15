ebeb508ef0b2f99a2d9a1eee695568f823ef14e1
dvdrtrgn <dvdrtrgn@gmail.com>
2015-10-15 15:13:32 -0500

    first commit

---
+++ app/config.js
@@ -0,0 +1,62 @@
+/*jslint white:false */
+/*global require, window */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+var W = (W && W.window || window), C = (W.C || W.console || {});
+
+W.SHIET = {};
+W.debug = Number(new Date('2015/10/01') > new Date());
+
+require.config({
+    baseUrl: 'scripts',
+    paths: {
+        lr: 'http://localhost:7325/livereload.js?snipver=1',
+        lib: 'libs',
+        ven: '../vendor',
+        jquery: '/lib/jquery/1.8.2/jquery',
+        lodash: '/lib/underscore/js-1.4.4/lodash.underscore',
+        modern: '/lib/modernizr/2.6.2/modernizr.min',
+        videojs: '/lib/video-js/ecg/video',
+        //
+        beacon: 'libs/ecg-beacon',
+        console: 'libs/console',
+        modal: 'libs/modal',
+        stats: 'libs/ecg-stats',
+        //
+    }
+});
+
+require(['modern', 'console'], function () {
+    try {
+        W.SHIET.init();
+
+        if (W.SHIET.trident) { // debug IE less
+            W.debug--;
+        }
+        if (W.location.hostname === 'localhost') { // debug local more
+            if (W.debug > 0) {
+                $('html').addClass('debug');
+            }
+            W.debug++;
+        }
+        if (W.debug > 0) { // any debug should attempt livereload
+            require(['lr']);
+            C.warn('LiveReloading');
+        }
+    } catch (err) {
+        C.error('config', err);
+    }
+
+    /// CUSTOM
+
+    require(['lodash', '_main'], function (_) {
+        _.delay(function () {
+            if (W.debug < 2) {
+                require(['stats'], function (stats) {
+                    stats.init('JUMBLE');
+                });
+            }
+        }, 1e3);
+    });
+});
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
---
+++ app/scripts/_main.js
@@ -0,0 +1,59 @@
+/*jslint white:false, laxcomma:true */
+/*global W, define */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ created drt 2015-09
+
+ USE
+ single use
+ hook up scripts to DOM
+
+ TODO
+
+ */
+define(['jquery', 'modal'], function
+    MAIN($, Modal) {
+    'use strict';
+
+    var Nom = 'Main';
+    var Main = {};
+    var W = (W && W.window || window), C = (W.C || W.console || {});
+    var Db = W.debug > 0;
+    var PC = !W.navigator.userAgent.match(/mobi/i);
+
+//EXTEND
+    $.scrollMain = function (px, ms) {
+        $('html,body').animate({scrollTop: px}, (ms || 999), 'swing');
+    };
+
+//  PRIVATE
+    function watchInputDevice() {
+        $('body').on('keydown', function () {
+            $(this).removeClass('mouse');
+            $(this).addClass('keyboard');
+        }).on('mousemove', function () {
+            $(this).removeClass('keyboard');
+            $(this).addClass('mouse');
+        });
+    }
+
+
+//  INIT
+    $(function () {
+        if (Db) {
+            W.main = Main;
+            Main.Modal = Modal; // expose for dev
+            C.info(Nom, 'init @', new Date(), 'debug:', Db, Main);
+        }
+
+        Main.mobile = !PC;
+        $.scrollMain(0); // reset page position
+
+        $('a.center').click(function (evt) {
+            evt.preventDefault();
+            $.scrollMain(333); // just to main area
+        });
+
+        watchInputDevice();
+    });
+
+});
