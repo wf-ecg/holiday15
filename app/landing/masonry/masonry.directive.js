f5b4d529fbec2949a17523b432655acafaa49934
dvdrtrgn <dvdrtrgn@gmail.com>
2015-10-20 16:47:43 -0500

    groom masonry

--- app/landing/masonry/masonry.directive.js
+++ app/landing/masonry/masonry.directive.js
@@ -11,28 +11,31 @@
 
             return {
                 compile: function (element, attrs) {
-                    // auto add animation to brick element
-                    var animation = attrs.ngAnimate || "'masonry'";
-                    var $brick = element.children();
-                    $brick.attr("ng-animate", animation);
+                    var animation, $brick, type, itemSelector;
 
+                    // auto add animation to brick element
+                    animation = attrs.ngAnimate || "'masonry'";
+                    $brick = element.children();
+                    $brick.attr("ng-animate", animation);
                     // generate item selector (exclude leaving items)
-                    var type = $brick.prop('tagName');
-                    var itemSelector = type + ":not([class$='-leave-active'])";
+                    type = $brick.prop('tagName');
+                    itemSelector = type + ":not([class$='-leave-active'])";
 
                     return function (scope, element, attrs) {
-                        var options = angular.extend({
+                        var options;
+
+                        options = angular.extend({
                             itemSelector: itemSelector
                         }, scope.$eval(attrs.masonry));
 
                         // try to infer model from ngRepeat
                         if (!options.model) {
                             var ngRepeatMatch = element.html().match(NGREPEAT_SOURCE_RE);
+
                             if (ngRepeatMatch) {
                                 options.model = ngRepeatMatch[4];
                             }
                         }
-
                         // initial animation
                         element.addClass('masonry');
 
@@ -47,13 +50,12 @@
                             if (options.model) {
                                 scope.$apply(function () {
                                     scope.$watchCollection(options.model, function (_new, _old) {
-                                        if (_new === _old) {
-                                            return;
+                                        if (_new !== _old) {
+                                            // Wait inside directives to render
+                                            setTimeout(function () {
+                                                element.masonry("reload");
+                                            });
                                         }
-                                        // Wait inside directives to render
-                                        setTimeout(function () {
-                                            element.masonry("reload");
-                                        });
                                     });
                                 });
                             }
