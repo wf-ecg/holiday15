68c9f56b3881fac3149009ff7e6055f741983aa8
dvdrtrgn <dvdrtrgn@gmail.com>
2015-11-09 17:25:11 -0600

    unpack freezers

--- app/vendor/ecg/utils.js
+++ app/vendor/ecg/utils.js
@@ -45,12 +45,14 @@ define(['jquery', 'lodash'], function ($, _) {
         return this.css({top: '', left: ''}).removeClass('freeze');
     };
     $.fn.freezeKids = function () {
-        return this.css({height: this.height(), width: this.width()}) //
-            .children().freeze();
+        this.css({height: this.height(), width: this.width()});
+        this.children().freeze();
+        return this;
     };
     $.fn.unfreezeKids = function () {
-        return this.css({height: '', width: ''}) //
-            .children().unfreeze();
+        this.css({height: '', width: ''});
+        this.children().unfreeze();
+        return this;
     };
 
 // WATCHERS
