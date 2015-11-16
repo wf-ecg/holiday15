9ca6d7deee4cfd861b59258a26616cdc1ad2b0ea
dvdrtrgn <dvdrtrgn@gmail.com>
2015-11-16 16:52:36 -0600

    try not to flash message

--- app/jingle/scripts/message.js
+++ app/jingle/scripts/message.js
@@ -69,14 +69,14 @@ define(['jquery'], function
                 return self;
             },
             show: function (cb) {
-                cf.ele.fadeTo(999, 1, function () {
+                cf.ele.stop(1,1).fadeTo(999, 1, function () {
                     if (typeof cb === 'function') cb();
                     $.publish('redraw');
                 });
                 return self;
             },
             write: function (str) {
-                cf.ele.fadeTo(0, 0.01).html(str);
+                cf.ele.stop(1,1).fadeTo(0, 0.01).html(str);
                 return self;
             },
             init: function () {
