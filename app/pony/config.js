f18b23770785a06f927938c335a193a027858d38
dvdrtrgn <dvdrtrgn@gmail.com>
2015-11-24 12:29:13 -0600

    add config

---
+++ app/pony/config.js
@@ -0,0 +1,67 @@
+/*jslint white:false */
+/*global require, window, _ */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+var W = (W && W.window || window),
+    C = (W.C || W.console || {});
+
+W.SHIET = {};
+W.debug = Number(new Date('2015/12/01') > new Date());
+
+require.config({
+    baseUrl: 'scripts',
+    paths: {
+        lr: 'http://localhost:7325/livereload.js?snipver=1',
+        jquery: '/lib/jquery/1.11.3/jquery',
+        jqmobi: '/lib/jquery/mobile/custom/jquery.mobile',
+        lodash: '/lib/underscore/js-1.4.4/lodash.underscore',
+        modern: '/lib/modernizr/2.6.2/modernizr',
+        //
+        data: '../../vendor/ecg/anagrams',
+        boots: '../../vendor/bootstrap/js/bootstrap.min',
+        console: '../../vendor/ecg/console',
+        modal: '../../vendor/ecg/modal',
+        utils: '../../vendor/ecg/utils',
+        //
+        beacon: 'libs/ecg-beacon',
+        jqxtn: 'libs/jq-xtn',
+        stats: 'libs/ecg-stats',
+        xtn: 'libs/drt-xtn',
+        //
+    },
+});
+
+require(['modern', 'console', 'lodash', 'utils'], function () {
+    try {
+        W.SHIET.init();
+
+        if (W.SHIET.trident) { // debug IE less
+            W.debug--;
+        }
+        if (W.location.hostname === 'localhost') { // debug local more
+            if (W.debug > 0) {
+                $('html').addClass('debug');
+            }
+            W.debug++;
+        }
+        if (W.debug > 0) { // any debug should attempt livereload
+            require(['lr']);
+            C.warn('LiveReloading');
+        }
+    } catch (err) {
+        C.error('config', err);
+    }
+
+    /// CUSTOM
+    require(['boots', 'jqmobi', '_main'], function () {
+
+        _.delay(function () {
+            if (W.debug < -2) {
+                require(['stats'], function (stats) {
+                    stats.init('HOLI-PONY');
+                });
+            }
+        }, 1e3);
+    });
+});
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
---
+++ app/pony/scripts/_main.js
@@ -0,0 +1,79 @@
+/*jslint white:false, laxcomma:true */
+/*global W, define */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ created drt 2015-09
+
+ USE
+ single use
+ hook up scripts to DOM
+
+ TODO
+
+ */
+define(['jquery', 'lodash', '../../vendor/misc/vendor', 'quizPannel'], function
+    MAIN($, _) {
+    'use strict';
+
+    var Nom = 'Main';
+    var Main = {};
+    var W = (W && W.window || window),
+        C = (W.C || W.console || {});
+
+    function db(num) {
+        return W.debug > (num || 1);
+    }
+    function expose(obj, log) {
+        if (db()) {
+            W.Main = Main; // expose for dev
+            $.extend(Main, obj);
+        }
+        if (log) {
+            C.info(Nom, 'expose', Main);
+        }
+    }
+
+//EXTEND
+    expose({
+    });
+    var header = $('header').first();
+    var pushin = $('.pushin').first();
+    var footer = $('footer').first();
+    var button;
+
+    header.load('../includes/main_header.html header > *', function () {
+        button = header.find('button').first();
+        button.click(function () {
+            $('.row-offcanvas').toggleClass('active');
+            button.toggleClass('collapsed');
+
+            if (button.is('.collapsed')) {
+                pushin.find('.shareBar ul').appendTo(header.find('.shareBar'));
+            } else {
+                header.find('.shareBar ul').appendTo(pushin.find('.shareBar'));
+            }
+        });
+        $.watchResize(function () {
+            if (!button.is('.collapsed')) {
+                button.click();
+            }
+        });
+    });
+
+    pushin.load('../includes/main_pushin.html .pushin > *');
+    footer.load('../includes/main_footer.html footer > *');
+
+    $.watchInputDevice();
+    $.markAgent();
+
+//  PRIVATE
+    function doBindings() {
+    }
+
+//  INIT
+    $(function () {
+        C.info(Nom, 'init @', new Date(), 'debug:', W.debug);
+
+        doBindings();
+    });
+
+});
