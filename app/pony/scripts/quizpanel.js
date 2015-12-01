4374c02ce92f0595a2d22992f661c3b198d80269
dvdrtrgn <dvdrtrgn@gmail.com>
2015-12-01 13:53:33 -0600

    scrub the scrum

--- app/pony/scripts/quizpanel.js
+++ app/pony/scripts/quizpanel.js
@@ -1,7 +1,7 @@
 /*jslint  white:false */
-/*globals define, window */
+/*global define, window */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-define(['jquery', 'hammer'], function ($, Hammer) {
+define(['jquery'], function ($) {
     var W, C;
 
     W = W || window;
@@ -9,16 +9,14 @@ define(['jquery', 'hammer'], function ($, Hammer) {
 
     var panelRatio = 0.84;
 
-    navigator.userAgent.match(/trident/i) && $('label').one('click', function () {
+    W.navigator.userAgent.match(/trident/i) && $('label').one('click', function () {
         $(this).find('input').click().trigger('change');
-        //C.log('foo gareth');
     });
 
 // Set close gradient classes on form panels
     function updatePanelClasses() {
         var panels = $('.panel');
         var newIndex = $('.panel-collapse').index($('.panel-collapse.in'));
-        //  var nextButton = $('.quiz-next');
 
         //drop old close-classes
         panels.removeClass('active');
@@ -31,7 +29,7 @@ define(['jquery', 'hammer'], function ($, Hammer) {
 
         // add close-# classes to control gradient fade
         panels.each(function (panelIndex) {
-            $(this).addClass("close-" + Math.abs(panelIndex - newIndex));
+            $(this).addClass('close-' + Math.abs(panelIndex - newIndex));
         });
     }
 
@@ -39,37 +37,33 @@ define(['jquery', 'hammer'], function ($, Hammer) {
     function updateProgressButtons() {
         var panels = $('.panel');
         var buttons = $('.progress-button');
+        var newIndex;
+
         //set answered flags on nav buttons
         panels.each(function (idx) {
-            if ($(this).find('input:checked').length)
-            {
+            if ($(this).find('input:checked').length) {
                 buttons.eq(idx).addClass('answered');
-            }
-            else
-            {
+            } else {
                 buttons.eq(idx).removeClass('answered');
             }
         });
 
-        var newIndex = $('.panel-collapse').index($('.panel-collapse.in'));
-        buttons.removeClass("active");
-        buttons.eq(newIndex).addClass("active");
+        newIndex = $('.panel-collapse').index($('.panel-collapse.in'));
+
+        buttons.removeClass('active');
+        buttons.eq(newIndex).addClass('active');
     }
 
     function advancePanel() {
         var current = $('.panel-collapse.in');
-        current.find('.quiz-next').trigger('click');
-    }
 
-    function retractPanel() {
-        var current = $('.panel-collapse.in');
-        current.find('.quiz-prev').trigger('click');
+        current.find('.quiz-next').trigger('click');
     }
 
     function focusPanel() {
         var current = $('.panel-collapse.in h2');
-        current.attr("tabIndex", -1).focus();
-        //console.log(current);
+
+        current.attr('tabIndex', -1).focus();
     }
 
     $(function () {
@@ -94,7 +88,7 @@ define(['jquery', 'hammer'], function ($, Hammer) {
             // Set the height of the panel to fil the screen, assuming a 10 pixel ribbon
             $('.panel-body').height(bodyheight * panelRatio);
             // Center the indicators
-            $('.progress-indicators').css("margin-top", "-" + progressCenter + "px");
+            $('.progress-indicators').css('margin-top', '-' + progressCenter + 'px');
         }
 
         quizInit();
@@ -107,162 +101,143 @@ define(['jquery', 'hammer'], function ($, Hammer) {
         });
 
         function displayCase() {
-            var ponies = ['eltoro', 'mollie', 'snowflake', 'king', 'nellie', 'buck', 'mack', 'prince', 'dandy', 'billy', 'trixie', 'grace', 'maggie', 'al'];
+            var ponies, rand, ponyName, pn;
 
-            var rand = Math.floor((Math.random() * ponies.length)); // returns a random number between 1 and 10
-            var ponyName = ponies[rand];
-            var pn = ponyName;
+            ponies = ['eltoro', 'mollie', 'snowflake', 'king', 'nellie', 'buck', 'mack', 'prince', 'dandy', 'billy', 'trixie', 'grace', 'maggie', 'al'];
+            rand = Math.floor((Math.random() * ponies.length)); // returns a random number between 1 and x
+            ponyName = ponies[rand];
+            pn = ponyName;
 
-            console.log(rand, ponies, pn);
-            //var epn =            var epb =            var epi =
+            function writeIt(a, b, c) {
+                $('#pn').html(a);
+                $('#pnBio').html(b);
+                $('#pnIcon').html(c + ' class="header-image-winner" alt="pony">');
+            }
 
             switch (pn) {
-                case "eltoro":
-                    document.getElementById("pn").innerHTML = "El Toro";
-                    document.getElementById("pnBio").innerHTML =
-                        "'The Bull' worked in Wells&nbsp;Fargo’s Mexico City headquarters. At 27 years of age, he was oldest horse in the stables and only worked a few hours a week to keep him&nbsp;limber.";
-                    document.getElementById("pnIcon").innerHTML =
-                        "<img src='./images/icons/pony/eltoro.svg' class='header-image-winner' alt='pony'>";
+                case 'eltoro':
+                    writeIt('El Toro',
+                        '‘The Bull’ worked in Wells&nbsp;Fargo’s Mexico City headquarters. ' +
+                        'At 27 years of age, he was oldest horse in the stables and only worked a few hours a week to keep him&nbsp;limber.',
+                        '<img src="./images/icons/pony/eltoro.svg"');
                     break;
-                case "mollie":
-                    document.getElementById("pn").innerHTML = "Mollie";
-                    document.getElementById("pnBio").innerHTML =
-                        "A beautiful white mare that led the stagecoach team that brought stagecoach President Hayes to Roseburg, Oregon, narrowly missing a run-in with the notorious bandit, Black&nbsp;Bart.";
-                    document.getElementById("pnIcon").innerHTML =
-                        "<img src='./images/icons/pony/mollie.svg' class='header-image-winner' alt='pony'>";
+                case 'mollie':
+                    writeIt('Mollie',
+                        'A beautiful white mare that led the stagecoach team that brought stagecoach President Hayes to Roseburg, ' +
+                        'Oregon, narrowly missing a run-in with the notorious bandit, Black&nbsp;Bart.',
+                        '<img src="./images/icons/pony/mollie.svg"');
                     break;
-                case "snowflake":
-                    document.getElementById("pn").innerHTML = "Snowflake";
-                    document.getElementById("pnBio").innerHTML =
-                        "A pristine white horse, Snowflake lived in New Jersey and rode a ferry across the Hudson every morning to pull a wagon throughout&nbsp;Manhattan. ";
-                    document.getElementById("pnIcon").innerHTML =
-                        "<img src='./images/icons/pony/snowflake.svg' class='header-image-winner' alt='pony'>";
+                case 'snowflake':
+                    writeIt('Snowflake',
+                        'A pristine white horse, Snowflake lived in New Jersey and rode a ferry across the Hudson ' +
+                        'every morning to pull a wagon throughout&nbsp;Manhattan. ',
+                        '<img src="./images/icons/pony/snowflake.svg"');
                     break;
-                case "king":
-                    document.getElementById("pn").innerHTML = "King";
-                    document.getElementById("pnBio").innerHTML =
-                        "A black horse, King faithfully served Wells&nbsp;Fargo in Las Vegas, New Mexico, and Los&nbsp;Angeles.";
-                    document.getElementById("pnIcon").innerHTML =
-                        "<img src='./images/icons/pony/king.svg' class='header-image-winner' alt='pony'>";
+                case 'king':
+                    writeIt('King',
+                        'A black horse, King faithfully served Wells&nbsp;Fargo in Las Vegas, New Mexico, and Los&nbsp;Angeles.',
+                        '<img src="./images/icons/pony/king.svg"');
                     break;
-                case "nellie":
-                    document.getElementById("pn").innerHTML = "Nellie";
-                    document.getElementById("pnBio").innerHTML =
-                        "With blazing speed, Nellie helped get her driver’s sick brother to a doctor and then finished her Wells&nbsp;Fargo&nbsp;route.";
-                    document.getElementById("pnIcon").innerHTML =
-                        "<img src='./images/icons/pony/nellie.svg' class='header-image-winner' alt='pony'>";
+                case 'nellie':
+                    writeIt('Nellie',
+                        'With blazing speed, Nellie helped get her driver’s sick brother to a doctor and then finished her Wells&nbsp;Fargo&nbsp;route.',
+                        '<img src="./images/icons/pony/nellie.svg"');
                     break;
-                case "buck":
-                    document.getElementById("pn").innerHTML = "Buck";
-                    document.getElementById("pnBio").innerHTML =
-                        "Handsome Buck started working for Wells&nbsp;Fargo in 1900 in Ardmore Oklahoma Territory. White he wasn’t the largest horse in the stable, Buck could pull a fully-loaded express wagon all by&nbsp;himself.";
-                    document.getElementById("pnIcon").innerHTML =
-                        "<img src='./images/icons/pony/buck.svg' class='header-image-winner' alt='pony'>";
+                case 'buck':
+                    writeIt('Buck',
+                        'Handsome Buck started working for Wells&nbsp;Fargo in 1900 in Ardmore Oklahoma Territory.' +
+                        'White he wasn’t the largest horse in the stable, Buck could pull a fully-loaded express wagon all by&nbsp;himself.',
+                        '<img src="./images/icons/pony/buck.svg"');
                     break;
-                case "mack":
-                    document.getElementById("pn").innerHTML = "Mack";
-                    document.getElementById("pnBio").innerHTML =
-                        "One of the most beloved horses that ever worked for Wells&nbsp;Fargo, the chestnut-colored Mack pulled a stagecoach on the Pioneer Stage Line from Placerville, California to Carson City,&nbsp;Nevada.";
-                    document.getElementById("pnIcon").innerHTML =
-                        "<img src='./images/icons/pony/mack.svg' class='header-image-winner' alt='pony'>";
+                case 'mack':
+                    writeIt('Mack',
+                        'One of the most beloved horses that ever worked for Wells&nbsp;Fargo, the chestnut-colored Mack ' +
+                        'pulled a stagecoach on the Pioneer Stage Line from Placerville, California to Carson City,&nbsp;Nevada.',
+                        '<img src="./images/icons/pony/mack.svg"');
                     break;
-                case "prince":
-                    document.getElementById("pn").innerHTML = "Prince";
-                    document.getElementById("pnBio").innerHTML =
-                        "A striking gray horse that got his name because he was the most handsome horse in Ardmore, Oklahoma Territory stables and because he cost the princely sum of&nbsp;$125.";
-                    document.getElementById("pnIcon").innerHTML =
-                        "<img src='./images/icons/pony/prince.svg' class='header-image-winner' alt='pony'>";
+                case 'prince':
+                    writeIt('Prince',
+                        'A striking gray horse that got his name because he was the most handsome horse in Ardmore, ' +
+                        'Oklahoma Territory stables and because he cost the princely sum of&nbsp;$125.',
+                        '<img src="./images/icons/pony/prince.svg"');
                     break;
-                case "dandy":
-                    document.getElementById("pn").innerHTML = "Dandy";
-                    document.getElementById("pnBio").innerHTML =
-                        "A big bay horse with a white mane, Dandy was one of the most valuable and expensive horses in 1895 Auburn, California. Wells&nbsp;Fargo paid $18 – top dollar in those days – to get&nbsp;him.";
-                    document.getElementById("pnIcon").innerHTML =
-                        "<img src='./images/icons/pony/dandy.svg' class='header-image-winner' alt='pony'>";
+                case 'dandy':
+                    writeIt('Dandy',
+                        'A big bay horse with a white mane, Dandy was one of the most valuable and expensive horses in 1895 Auburn, California. ' +
+                        'Wells&nbsp;Fargo paid $18 – top dollar in those days – to get&nbsp;him.',
+                        '<img src="./images/icons/pony/dandy.svg"');
                     break;
-                case "billy":
-                    document.getElementById("pn").innerHTML = "Billy";
-                    document.getElementById("pnBio").innerHTML =
-                        "A trusty Appaloosa and an early media star featured in the February 1914 issue of the Wells&nbsp;Fargo&nbsp;Messenger.";
-                    document.getElementById("pnIcon").innerHTML =
-                        "<img src='./images/icons/pony/billy.svg' class='header-image-winner' alt='pony'>";
+                case 'billy':
+                    writeIt('Billy',
+                        'A trusty Appaloosa and an early media star featured in the February 1914 issue of the Wells&nbsp;Fargo&nbsp;Messenger.',
+                        '<img src="./images/icons/pony/billy.svg"');
                     break;
-                case "trixie":
-                    document.getElementById("pn").innerHTML = "Trixie";
-                    document.getElementById("pnBio").innerHTML =
-                        "A fine brown and white Paint, Trixie was born and raised in Ardmore, Oklahoma Territory and started working for Wells&nbsp;Fargo in 1918, often donning a wool blanket in the winter months to stay&nbsp;warm.";
-                    document.getElementById("pnIcon").innerHTML =
-                        "<img src='./images/icons/pony/trixie.svg' class='header-image-winner' alt='pony'>";
+                case 'trixie':
+                    writeIt('Trixie',
+                        'A fine brown and white Paint, Trixie was born and raised in Ardmore, Oklahoma Territory and started ' +
+                        'working for Wells&nbsp;Fargo in 1918, often donning a wool blanket in the winter months to stay&nbsp;warm.',
+                        '<img src="./images/icons/pony/trixie.svg"');
                     break;
-                case "grace":
-                    document.getElementById("pn").innerHTML = "Grace";
-                    document.getElementById("pnBio").innerHTML =
-                        "Had a reputation for delivering customers express shipments in Los Angeles with a dignified manner and  was one of the last ponies to ever work for Wells&nbsp;Fargo.";
-                    document.getElementById("pnIcon").innerHTML =
-                        "<img src='./images/icons/pony/grace.svg' class='header-image-winner' alt='pony'>";
+                case 'grace':
+                    writeIt('Grace',
+                        'Had a reputation for delivering customers express shipments in Los Angeles with a dignified manner and was ' +
+                        'one of the last ponies to ever work for Wells&nbsp;Fargo.',
+                        '<img src="./images/icons/pony/grace.svg"');
                     break;
-                case "maggie":
-                    document.getElementById("pn").innerHTML = "Maggie";
-                    document.getElementById("pnBio").innerHTML =
-                        "A brown mare with over 20 years service that represented Wells&nbsp;Fargo in the first annual San Francisco Work Horse Parade in&nbsp;1909.";
-                    document.getElementById("pnIcon").innerHTML =
-                        "<img src='./images/icons/pony/maggie.svg' class='header-image-winner' alt='pony'>";
+                case 'maggie':
+                    writeIt('Maggie',
+                        'A brown mare with over 20 years service that represented Wells&nbsp;Fargo in the first annual San Francisco Work Horse Parade in&nbsp;1909.',
+                        '<img src="./images/icons/pony/maggie.svg"');
                     break;
-                case "al":
-                    document.getElementById("pn").innerHTML = "Al";
-                    document.getElementById("pnBio").innerHTML =
-                        "So beloved by his driver, Al was bought by the driver and retired to the driver’s ranch to live out the rest of his days in peace and&nbsp;quiet.";
-                    document.getElementById("pnIcon").innerHTML =
-                        "<img src='./images/icons/pony/al.svg' class='header-image-winner' alt='pony'>";
+                case 'al':
+                    writeIt('Al',
+                        'So beloved by his driver, Al was bought by the driver and retired to the driver’s ranch to live out the rest of his days in peace and&nbsp;quiet.',
+                        '<img src="./images/icons/pony/al.svg"');
                     break;
             }
         }
 
         $('#QuizWelliFa input').change(function () {
             //advance to next Question
-
             advancePanel();
             focusPanel();
-
-            var result_page = $('#results-container');
 
         });
 
         $('#newQuiz').click(function () {
-            location.reload();
+            W.location.reload();
         });
 
         $('.filler').click(function () {
             var target = $(this).parent().find('input');
+
             target.prop('checked', true);
             target.change();
         });
 
         $('#question-10 .responses').one('click', 'input:radio', function () {
-            C.log("I'm getting the pony");
+            var bodyheight, panels, panelheight, vheight, vwidth;
 
             $('#myQuiz').removeClass('visible').addClass('hidden');
             $('#results-container').removeClass('hidden').addClass('visible');
 
-            var panels = $('.panel');
-            var vwidth = $(W).width();
+            panels = $('.panel');
+            vwidth = $(W).width();
+
             if (vwidth < 768) {
-                var panelheight = 0;
-                var vheight = W.innerHeight;
+                panelheight = 0;
+                vheight = W.innerHeight;
             } else {
-                var panelheight = 0;
-                var vheight = $(W).height();
+                panelheight = 0;
+                vheight = $(W).height();
             }
-            var bodyheight = vheight - (panels.length * panelheight) + panelheight;
+            bodyheight = vheight - (panels.length * panelheight) + panelheight;
 
             // Set the height of the panel to fil the screen, assuming a 10 pixel ribbon
             $('.panel-body').height(bodyheight * panelRatio);
 
             displayCase();
         });
-
-        //var hammertime = new Hammer(document.body, {preventDefault: false});
-        //hammertime.get('swipe').set({direction: Hammer.DIRECTION_VERTICAL});
 
     });
 
