464137d68b1fdc0d304900936f666b1c741372d9
dvdrtrgn <dvdrtrgn@gmail.com>
2015-12-04 15:27:07 -0600

    add enterprise jumble

---
+++ app/jumbles/_main.js
@@ -0,0 +1,205 @@
+/*jslint white:false, laxcomma:true */
+/*global W, define */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ created drt 2015-09
+
+ USE
+ single use
+ hook up scripts to DOM
+
+ TODO
+
+ */
+define(['jquery', 'lodash', 'modal', 'timer', 'game', 'message', 'share'], function
+    MAIN($, _, Modal, Timer, Game, Message, Share) {
+    'use strict';
+
+    var Nom = 'Main';
+    var Main = {};
+    var W = (W && W.window || window),
+        C = (W.C || W.console || {});
+
+    // - - - - - - - - - - - - - - - - - -
+    // EXTEND
+    var ACT = 'keypress click';
+    var totalWon;
+    var duration = 120;
+    var game, message, timer;
+
+    var El = {
+        intro: '.intro',
+        outro: '.outro',
+        jumble: '.jumble',
+        start: '.intro button',
+        again: '.outro button',
+        score: '.status .score',
+        header: 'header',
+        rating: '.ratings',
+    };
+
+    // repair page determination
+    if (W.location.hash.slice(1) === 'wystar') {
+        W.location.href = 'wystar.html';
+    }
+
+    $.watchResize(function () {
+        var ua = W.navigator.userAgent;
+        if (ua.match(/mobi/i) ||
+            $(W).width() < 768) { // simulate
+            $('html').removeClass('desktop');
+            $('html').addClass('mobile');
+        } else {
+            $('html').removeClass('mobile');
+            $('html').addClass('desktop');
+        }
+        if (ua.match(/chrome/i)) {
+            $('html').addClass('chrome');
+        } else if (ua.match(/safari/i)) {
+            $('html').addClass('safari');
+        } else if (ua.match(/firefox/i)) {
+            $('html').addClass('firefox');
+        } else if (ua.match(/trident/i)) {
+            $('html').addClass('trident');
+        }
+    }, 'markAgent');
+    $.swallowBackspace();
+    $.watchInputDevice();
+
+    // - - - - - - - - - - - - - - - - - -
+    // PRIVATE
+    function db(num) {
+        return W.debug > (num || 0);
+    }
+    function expose(obj) {
+        if (db(0)) {
+            W.Main = Main; // expose for console
+            $.extend(Main, obj);
+            C.info(Nom, 'expose', obj);
+        }
+    }
+
+    function checkPath() {
+        // determine if we are doing an IRT site
+        // apply footer and header and pushin appropriate
+        // also change the GA token
+        //
+    }
+
+    // - - - - - - - - - - - - - - - - - -
+    // WIRING
+    function updateScore(score) {
+        El.score.html('Score: ' + (score || 0));
+    }
+    function showRating(score) {
+        var rating = '';
+
+        El.outro.find('.score').text(score);
+        El.rating.find('span').removeClass('active');
+
+        if (score > 4) {
+            rating = 'good';
+        } else if (score > 2) {
+            rating = 'okay';
+        } else if (score > 0) {
+            rating = 'lame';
+        }
+        if (rating) {
+            El.rating.find('.' + rating).addClass('active');
+        }
+        Share.tweak(score, rating);
+    }
+
+    function hideAreas() {
+        El.jumble.hide();
+        El.intro.hide();
+        El.outro.hide();
+    }
+    function showIntro() {
+        //El.outro.find('.shareBar ul').appendTo(El.header.find('.shareBar'));
+
+        hideAreas();
+        showRating(0);
+        El.intro.show();
+        timer.force('Start');
+    }
+    function showOutro() {
+        //El.header.find('.shareBar ul').appendTo(El.outro.find('.shareBar'));
+
+        timer.stop();
+        hideAreas();
+        El.outro.show();
+        showRating(totalWon);
+        timer.reset();
+    }
+    function showJumble() {
+        message.init();
+        totalWon = 0;
+        updateScore();
+        hideAreas();
+        El.jumble.show();
+        timer.start(duration);
+        game.start();
+    }
+
+//  INIT
+    function doBindings() {
+        $.doneLoading();
+        $.watchHash();
+        $.reify(El);
+
+        El.start.on(ACT, showJumble);
+        El.score.click(function () {
+            if (db()) {
+                $.publish('finish.Timer');
+            }
+        });
+        El.again.on(ACT, showIntro);
+
+        game = new Game();
+        message = new Message();
+        timer = new Timer({
+            bottom: -1,
+            warn: 9,
+            div: '.game .timer',
+        });
+
+        expose({
+            Modal: Modal,
+            Share: Share,
+            message: message,
+            game: game,
+            timer: timer,
+        });
+
+        $.subscribe('finish.Timer', function () {
+            showOutro();
+        });
+        $.subscribe('expose.Main', function () {
+            expose(arguments[1]);
+        });
+        $.subscribe('win.Game', function () {
+            message.cheer();
+            updateScore(++totalWon);
+            timer.stop();
+        });
+        $.subscribe('next.Game', function () {
+            message.init(); // show instructions
+            timer.add(1).start();  // restart the timer
+        });
+
+        showIntro();
+    }
+
+    $(function () {
+        C.info(Nom, 'init @', new Date(), 'debug:', W.debug);
+        //require(['tests/timer.test']);
+        doBindings();
+    });
+
+});
+
+/*
+
+
+
+ */
---
+++ app/jumbles/config.js
@@ -0,0 +1,69 @@
+/*jslint white:false */
+/*global require, window, _ */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+var W = (W && W.window || window),
+    C = (W.C || W.console || {});
+
+W.SHIET = {};
+W.debug = Number(new Date('2015/12/01') > new Date());
+
+require.config({
+    baseUrl: '../jingle-jumbles/scripts',
+    paths: {
+        lr: 'http://localhost:7325/livereload.js?snipver=1',
+        jquery: '/lib/jquery/1.11.3/jquery',
+        jqmobi: '/lib/jquery/mobile/custom/jquery.mobile',
+        lodash: '/lib/underscore/js-1.4.4/lodash.underscore',
+        modern: '/lib/modernizr/2.6.2/modernizr',
+        //
+        data: '../../vendor/ecg/anagrams',
+        boots: '../../vendor/bootstrap/js/bootstrap.min',
+        console: '../../vendor/ecg/console',
+        dialog: '../../vendor/ecg/dialog',
+        modal: '../../vendor/ecg/modal',
+        utils: '../../vendor/ecg/utils',
+        beacon: '../../vendor/ecg/beacon',
+        stats: '../../vendor/ecg/stats',
+        //
+        jqxtn: 'libs/jq-xtn',
+        xtn: 'libs/drt-xtn',
+        //
+    },
+});
+
+require(['modern', 'console', 'lodash', 'utils'], function () {
+    var html = $('html');
+    try {
+        W.SHIET.init();
+
+        if (W.SHIET.trident) { // debug IE less
+            W.debug--;
+        }
+        if (W.location.hostname === 'localhost') { // debug local more
+            if (W.debug > 0) {
+                $('html').addClass('debug');
+            }
+            W.debug++;
+        }
+        if (W.debug > 0) { // any debug should attempt livereload
+            require(['lr']);
+            C.warn('LiveReloading');
+        }
+    } catch (err) {
+        C.error('config', err);
+    }
+
+    /// CUSTOM
+    require(['boots', 'jqmobi', 'jqxtn', '../../jumbles/_main'], function () {
+
+        _.delay(function () {
+            if (W.debug < 2) {
+                require(['stats'], function (stats) {
+                    stats.init('HOLI-JING');
+                });
+            }
+        }, 1e3);
+    });
+});
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
